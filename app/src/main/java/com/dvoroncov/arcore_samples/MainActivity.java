fb95aafa96f52af864c6860566ca1a6e44327463
dvoroncov <dvoroncov@htc-cs.ru>
2018-07-27 19:20:09 +0400

    planetary_crawler

--- app/src/main/java/com/dvoroncov/arcore_samples/MainActivity.java
+++ app/src/main/java/com/dvoroncov/arcore_samples/MainActivity.java
@@ -1,60 +1,103 @@
 package com.dvoroncov.arcore_samples;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
-import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.math.Quaternion;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.Light;
+import com.google.ar.sceneform.rendering.MaterialFactory;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.PlaneRenderer;
 import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.rendering.ShapeFactory;
+import com.google.ar.sceneform.rendering.Texture;
 import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
-import com.google.ar.sceneform.ux.BaseArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
     private ArFragment arFragment;
-    private Renderable renderable;
+    private Button buttonX;
+
+    private ViewRenderable viewRenderable;
+    private ModelRenderable amenemhatRenderable;
+    private ModelRenderable planetaryCrawlerRenderable;
+    private ModelRenderable redSphereRenderable;
+
+    private TransformableNode node;
+    private Anchor anchor;
+    private AnchorNode anchorNode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ar_fragment);
+        buttonX = findViewById(R.id.button_x);
+        //buttonX.setOnClickListener(v -> {
+        //    if (anchorNode != null) {
+        //        Vector3 position = anchorNode.getLocalPosition();
+        //        position.x += 0.1;
+        //        anchorNode.setLocalPosition(position);
+        //        anchorNode.setEnabled(true);
+        //    }
+        //});
 
-        //ModelRenderable.builder()
-        //        .setSource(this, Uri.parse("amenemhat.sfb"))
-        //        .build()
-        //        .thenAccept(renderable -> MainActivity.this.renderable = renderable)
-        //        .exceptionally(throwable -> {
-        //            Toast toast =
-        //                    Toast.makeText(this, "Unable to load amenemhat renderable", Toast.LENGTH_LONG);
-        //            toast.setGravity(Gravity.CENTER, 0, 0);
-        //            toast.show();
-        //            return null;
-        //        });
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("amenemhat.sfb"))
+                .build()
+                .thenAccept(viewRenderable -> MainActivity.this.amenemhatRenderable = viewRenderable)
+                .exceptionally(throwable -> {
+                    Toast toast =
+                            Toast.makeText(this, "Unable to load amenemhat viewRenderable", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    return null;
+                });
+
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("planetary_crawler.sfb"))
+                .build()
+                .thenAccept(viewRenderable -> MainActivity.this.planetaryCrawlerRenderable = viewRenderable)
+                .exceptionally(throwable -> {
+                    Toast toast =
+                            Toast.makeText(this, "Unable to load amenemhat viewRenderable", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    return null;
+                });
 
         ViewRenderable.builder()
                 .setView(this, R.layout.ar_view)
                 .build()
-                .thenAccept(renderable -> MainActivity.this.renderable = renderable)
+                .thenAccept(renderable -> MainActivity.this.viewRenderable = renderable)
                 .exceptionally(throwable -> {
-                    Toast toast =
-                            Toast.makeText(this, "Unable to load amenemhat renderable", Toast.LENGTH_LONG);
+                    Toast toast = Toast.makeText(this, "Unable to load amenemhat viewRenderable", Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.CENTER, 0, 0);
                     toast.show();
                     return null;
                 });
 
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
-            if (renderable == null) {
+            if (planetaryCrawlerRenderable == null) {
                 return;
             }
 
@@ -62,14 +105,26 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
 
-            Anchor anchor = hitResult.createAnchor();
-            AnchorNode anchorNode = new AnchorNode(anchor);
-            anchorNode.setParent(arFragment.getArSceneView().getScene());
+            if (anchor == null) {
+                planetaryCrawlerRenderable.setShadowCaster(true);
 
-            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
-            node.setParent(anchorNode);
-            node.setRenderable(renderable);
-            node.select();
+                anchor = hitResult.createAnchor();
+                //anchorNode = new AnchorNode(anchor);
+                //anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                RotatingNode planetVisual = new RotatingNode(0, 1, false);
+                planetVisual.setParent(arFragment.getArSceneView().getScene());
+                planetVisual.setRenderable(planetaryCrawlerRenderable);
+
+                //Button button = viewRenderable.getView().findViewById(R.id.ar_button);
+                //TextView textView = viewRenderable.getView().findViewById(R.id.ar_text_view);
+                //button.setOnClickListener(v -> textView.setTextColor(getResources().getColor(android.R.color.holo_red_light)));
+
+                //node = new TransformableNode(arFragment.getTransformationSystem());
+                //node.setParent(anchorNode);
+                //node.setRenderable(planetaryCrawlerRenderable);
+                //node.select();
+            }
         });
     }
 }
